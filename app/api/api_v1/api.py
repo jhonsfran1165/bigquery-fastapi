721ba83e6474bd5571b5de70a8628ad129c5b970
Sebastian Franco <jsfranco@whaleandjaguar.co>
2021-09-03 20:06:01 -0500

    chore: initial commit

---
+++ app/api/api_v1/api.py
@@ -0,0 +1,7 @@
+from fastapi import APIRouter
+from app.api.api_v1.endpoints import login, items, utils
+
+api_router = APIRouter()
+api_router.include_router(login.router, tags=["login"])
+api_router.include_router(items.router, prefix="/items", tags=["items"])
+api_router.include_router(utils.router, prefix="/utils", tags=["utils"])
